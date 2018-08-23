08c8b104fd4204dd0c190351023bac5f8caf256a
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-08-23 21:48:54 +0900

    Update: settings.py
    rest_framework 모듈 사용 설정

--- backend/stufolio/settings.py
+++ backend/stufolio/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #pre-installed
+    'rest_framework'
+    #user-installed
 ]
 
 MIDDLEWARE = [
