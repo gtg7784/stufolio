a263a8a53c3d11e493bb42476e1ef337dc0f533a
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-08-23 21:51:14 +0900

    Create: app api
    api를 담당하는 Django앱 생성

--- backend/stufolio/settings.py
+++ backend/stufolio/settings.py
@@ -41,8 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #pre-installed
-    'rest_framework'
+    'rest_framework',
     #user-installed
+    'api',
+    #user-app
 ]
 
 MIDDLEWARE = [
