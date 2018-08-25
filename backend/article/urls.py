28d705f9527af4b51374ff2993a5818ff453e8b6
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-08-26 00:16:11 +0900

    Update: urls.py
    '하트' 기능에 접근하는 url을 /like/ 에서 /heart/ 로 변경

--- backend/article/urls.py
+++ backend/article/urls.py
@@ -3,6 +3,6 @@ from article import views
 
 urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.ArticleDetail.as_view()),
-    url(r'^(?P<pk>[0-9]+)/like/$', views.article_heart),
+    url(r'^(?P<pk>[0-9]+)/heart/$', views.article_heart),
     url(r'^$', views.ArticleList.as_view()),
 ]
