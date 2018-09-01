e00fadc2ce91c4e17ec1b90ac213b4b99f9fc69c
Yeon-Gyu-Kim <11153873+kim-yeon-gyu-exlock@users.noreply.github.com>
2018-09-01 10:06:11 +0900

    Update: models.py
    필요없는 모델 목록 삭제

--- backend/custom_profile/models.py
+++ backend/custom_profile/models.py
@@ -2,18 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from article.models import Article
 
-
-class Block(models.Model):
-    date = models.DateField(auto_now_add=True)
-    article = models.ManyToManyField(Article)
-
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=400, blank=True)
     school = models.CharField(max_length=20, blank=True)
     image = models.ImageField()
-    #기본 소개
-    num_of_articles = models.IntegerField(null=False, default=0)
-    num_of_total_hearts = models.IntegerField(null=False, default=0)
-    block = models.ManyToManyField(Block)
