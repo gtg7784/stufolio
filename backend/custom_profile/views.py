4bcfa88658c1532ba92f5517fa9354ba47330184
Yeon-Gyu-Kim <11153873+kim-yeon-gyu-exlock@users.noreply.github.com>
2018-09-01 12:55:07 +0900

    Update: views.py
    프로필 기능 추가

--- backend/custom_profile/views.py
+++ backend/custom_profile/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+from custom_profile.models import Profile
+from custom_profile.serializers import ProfileSerializer
+
+
+class Profile(generics.RetrieveUpdateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permissions = (permissions.IsAuthenticatedOrReadOnly)
