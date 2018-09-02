0864894494759c142f5a5c869828b4d5962931e3
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-09-02 23:56:02 +0900

    Update: models.py
    ImageField를 ProcessedImageField로, 저장 디렉토리를 static/uploaded/images/profile/%Y/%m/%d 로 변경

--- backend/custom_profile/models.py
+++ backend/custom_profile/models.py
@@ -1,9 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
+from imagekit.models import ProcessedImageField
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     bio = models.CharField(max_length=400, blank=True)
     school = models.CharField(max_length=20, blank=True)
-    image = models.ImageField()
+    image = ProcessedImageField(
+        upload_to='static/uploaded/images/profile/%Y/%m/%d/',
+        format='JPEG',
+        options={'quality': 70})
