bb2f9db786d9caec670d9a9c23930946e348657d
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-09-03 00:29:28 +0900

    Update: urls.py
    Comma 추가로 문법 오류 해결

--- backend/custom_profile/urls.py
+++ backend/custom_profile/urls.py
@@ -3,6 +3,6 @@ from custom_profile import views
 
 urlpatterns = [
     url(r'^$', views.ProfileOverall.as_view()),
-    url(r'^(?P<string>[\w\-]+)/$', views.ProfileDetail.as_view())
+    url(r'^(?P<string>[\w\-]+)/$', views.ProfileDetail.as_view()),
     url(r'^image/(?P<string>[\w\-]+)/$', views.ProfileDetail.as_view())
 ]
