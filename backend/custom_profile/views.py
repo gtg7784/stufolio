f34eaecf4e7cc7bed29e52aea8e5b6abc6bd8a96
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-09-02 21:43:15 +0900

    Refactor: rename class name Profile to ProfileDetail

--- backend/custom_profile/views.py
+++ backend/custom_profile/views.py
@@ -4,7 +4,7 @@ from custom_profile.models import Profile
 from custom_profile.serializers import ProfileSerializer
 
 
-class Profile(generics.RetrieveUpdateAPIView):
+class ProfileDetail(generics.RetrieveUpdateAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permissions = (permissions.IsAuthenticatedOrReadOnly)
