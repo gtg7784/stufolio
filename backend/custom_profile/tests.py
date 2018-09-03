996ff0970a4c92f4fe42b617c0528ff6ad59a917
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-09-03 23:26:21 +0900

    Update: tests.py
    프로필 기능 테스트 추가

--- backend/custom_profile/tests.py
+++ backend/custom_profile/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from urllib.parse import urlencode, quote_plus
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+
+class TestProfile(TestCase):
+    def setUp(self):
+        self.username = 'testing_account'
+        self.password = 'for_the_test'
+        self.email = 'testemail@somethingdoesntexists.com'
+        self.client = Client()
+
+    def test_sign_up(self):
+        params = {
+            "username": self.username,
+            "email": self.email,
+            "password1": self.password,
+            "password2": self.password
+        }
+        params = urlencode(params, quote_via=quote_plus)
+        response = self.client.post(
+            '/profiles/signup/',
+            data=params,
+            content_type="application/x-www-form-urlencoded")
+        assert response.status_code is 201
