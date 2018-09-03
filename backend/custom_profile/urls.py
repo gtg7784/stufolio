804445fda2883d50df652b23b3bd1801effe2b4b
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-09-03 23:27:26 +0900

    Update: urls.py
    충돌을 야기하는 url patterns 우선순위 변경

--- backend/custom_profile/urls.py
+++ backend/custom_profile/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from custom_profile import views
 
 urlpatterns = [
-    url(r'^$', views.ProfileOverall.as_view()),
-    url(r'^(?P<string>[\w\-]+)/$', views.ProfileDetail.as_view()),
     url(r'^image/(?P<string>[\w\-]+)/$', views.ProfileDetail.as_view()),
-    url(r'^signup/$', views.sign_up)
+    url(r'^signup/$', views.sign_up),
+    url(r'^(?P<string>[\w\-]+)/$', views.ProfileDetail.as_view()),
+    url(r'^$', views.ProfileOverall.as_view()),
 ]
