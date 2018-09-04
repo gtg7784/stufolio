620c3092dea2b9f98b30ad78860ecd08f905255f
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-09-04 23:46:36 +0900

    Add: paging feature

--- backend/article/urls.py
+++ backend/article/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from article import views
 
 urlpatterns = [
+    url(r'^pages/(?P<pk>[0-9]+)/$', views.get_articles_page)
     url(r'^images/(?P<pk>[0-9]+)/$', views.image),
     url(r'^images/$', views.ImageCreation.as_view()),
     url(r'^user/(?P<string>[\w\-]+)/$', views.article_profile),
--- backend/article/views.py
+++ backend/article/views.py
@@ -1,6 +1,7 @@
 import json
 
 from django.http import Http404, JsonResponse, HttpResponse
+from django.core.paginator import Paginator, EmptyPage
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 
@@ -28,20 +29,21 @@ class ArticleList(generics.ListAPIView, APIView):
         except Image.DoesNotExist:
             return False
 
-    def post(self, request): # 작성자 이름 자동추가 기능을 위해 post용 뷰 분리
-        if request.user.is_authenticated: # 사용자가 인증 되었을경우
+    def post(self, request):  # 작성자 이름 자동추가 기능을 위해 post용 뷰 분리
+        if request.user.is_authenticated:  # 사용자가 인증 되었을경우
             serializer = ArticleSerializer(data=request.data)
-            for temp in json.loads(request.data.get('images_id')): # 이미지의 id값들이 유효한지 체크
-                if not self._is_image_available(temp): # 잘못된 id값을 받았을 경우
-                    return Response(
-                        status=status.HTTP_406_NOT_ACCEPTABLE)
+            for temp in json.loads(
+                    request.data.get('images_id')):  # 이미지의 id값들이 유효한지 체크
+                if not self._is_image_available(temp):  # 잘못된 id값을 받았을 경우
+                    return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
             if serializer.is_valid():
-                serializer.save(writer=request.user) # 작성자 요청자로 설정
+                serializer.save(writer=request.user)  # 작성자 요청자로 설정
                 return JsonResponse(
                     serializer.data, status=status.HTTP_201_CREATED)
             return Response(
-                serializer.errors, status=status.HTTP_400_BAD_REQUEST) # 폼에 오류가 있을 경우
-        return Response(status=status.HTTP_401_UNAUTHORIZED) #인증되지 않았을 경우
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST)  # 폼에 오류가 있을 경우
+        return Response(status=status.HTTP_401_UNAUTHORIZED)  #인증되지 않았을 경우
 
 
 class ArticleDetail(generics.RetrieveUpdateAPIView):
@@ -59,7 +61,13 @@ class ImageCreation(generics.CreateAPIView):
 
 
 @api_view(['GET'])
-def image(request, pk): # 이미지 반환
+def get_articles_page(request, pk):
+    articles = Article.objects.all().order_by('-created_at')
+    return Response(articles.values()[int(pk) * 10 - 10:int(pk) * 10])
+
+
+@api_view(['GET'])
+def image(request, pk):  # 이미지 반환
     test_file = open(
         settings.BASE_DIR + "/" + str(get_object_or_404(Image, pk=pk).image),
         'rb')
@@ -70,7 +78,7 @@ def image(request, pk): # 이미지 반환
 
 
 @api_view(['POST'])
-def article_heart(request, pk): # 게시글에 하트 추가
+def article_heart(request, pk):  # 게시글에 하트 추가
     article = get_object_or_404(Article, pk=pk)
     article_heart, article_heart_created = article.heart_set.get_or_create(
         user=request.user)
@@ -90,7 +98,7 @@ def article_heart(request, pk): # 게시글에 하트 추가
 
 
 @api_view(['GET'])
-def article_profile(request, string): #유저가 작성한 게시글을 반환
+def article_profile(request, string):  #유저가 작성한 게시글을 반환
     try:
         u = User.objects.get(username=string)
         articles = Article.objects.filter(writer=u).values()
