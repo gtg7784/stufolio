9843f957a6674e12b9a6d8a8d4986d5f85c189d4
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-09-04 21:49:06 +0900

    Update: views.py
    주석 추가

--- backend/article/views.py
+++ backend/article/views.py
@@ -28,7 +28,7 @@ class ArticleList(generics.ListAPIView, APIView):
         except Image.DoesNotExist:
             return False
 
-    def post(self, request):
+    def post(self, request): # 작성자 이름 자동추가 기능을 위해 post용 뷰 분리
         if request.user.is_authenticated: # 사용자가 인증 되었을경우
             serializer = ArticleSerializer(data=request.data)
             for temp in json.loads(request.data.get('images_id')): # 이미지의 id값들이 유효한지 체크
@@ -36,12 +36,12 @@ class ArticleList(generics.ListAPIView, APIView):
                     return Response(
                         status=status.HTTP_406_NOT_ACCEPTABLE)
             if serializer.is_valid():
-                serializer.save(writer=request.user)
+                serializer.save(writer=request.user) # 작성자 요청자로 설정
                 return JsonResponse(
                     serializer.data, status=status.HTTP_201_CREATED)
             return Response(
-                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST) # 폼에 오류가 있을 경우
+        return Response(status=status.HTTP_401_UNAUTHORIZED) #인증되지 않았을 경우
 
 
 class ArticleDetail(generics.RetrieveUpdateAPIView):
