3857a85e0dd70e7d84394ff2e590f50029a00eef
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-09-04 21:48:57 +0900

    Add: tag searching feature

--- backend/search/urls.py
+++ backend/search/urls.py
@@ -3,4 +3,5 @@ from search import views
 
 urlpatterns = [
     url(r'^profiles/(?P<string>[\w\-]+)/$', views.search_profiles),
+    url(r'^tags/(?P<string>[\w\-]+)/$', views.search_tags),
 ]
--- backend/search/views.py
+++ backend/search/views.py
@@ -1,10 +1,10 @@
-from article.models import Article
 from django.contrib.auth.models import User
-from custom_profile.models import Profile
 
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from article.models import Article
 
 # Search articles
 # - written by whom,
@@ -18,8 +18,29 @@ from rest_framework.response import Response
 def search_profiles(request, string):
     user = User.objects.filter(username__contains=string)
     result = []
-    print(user)
     for temp in user.values('username'):
         result += [temp]
+
+    return Response(result, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def search_tags(request, string):
+    result = {'articles': []}
+    for temp in Article.objects.filter(tags__contains=[string]):
+        article = {
+            'id': int(),
+            'writer': str(),
+            'content': str(),
+            'images_id': list(),
+            'tags': list()
+        }
+        article['id'] = temp.id
+        article['writer'] = temp.writer.username
+        article['content'] = temp.content
+        article['images_id'] = temp.images_id  # images_id 필드 자체로 배열
+        article['tags'] = temp.tags  # 태그 추가
+        print(article)
+        result.get('articles').append(article)
 
     return Response(result, status=status.HTTP_200_OK)
