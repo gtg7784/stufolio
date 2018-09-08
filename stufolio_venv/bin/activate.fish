a8de1d473f041b8d1f2f245eb114ccc3b9bd1296
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-09-08 14:45:35 +0900

    Refactor: virtualenv directory settings

--- stufolio_venv/bin/activate.fish
+++ stufolio_venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/exlock/git/Stufolio/backend/stufolio_venv"
+set -gx VIRTUAL_ENV "/home/exlock/git/Stufolio/stufolio_venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- stufolio_venv/bin/django-admin.py
+++ stufolio_venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/exlock/git/Stufolio/backend/stufolio_venv/bin/python3
+#!/home/exlock/git/Stufolio/stufolio_venv/bin/python3
 from django.core import management
 
 if __name__ == "__main__":
