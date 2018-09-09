9c4186c292bcb82f28986898f7bfa74a87612dd0
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-09-09 21:29:58 +0900

    Fix: social id and email id duplicates

--- backend/stufolio/settings.py
+++ backend/stufolio/settings.py
@@ -173,20 +173,15 @@ REST_FRAMEWORK = {
     ),
 }
 
-SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
-FACEBOOK_EXTENDED_PERMISSIONS = ['email', 'picture']
-SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
-    'fields': 'id, name, email, age_range'
-}
-
 SOCIAL_AUTH_PIPELINE = (
-    'social.pipeline.social_auth.social_details',
-    'social.pipeline.social_auth.social_uid',
-    'social.pipeline.social_auth.auth_allowed',
-    'social.pipeline.social_auth.social_user',
-    'social.pipeline.user.get_username',
-    'accounts.social.create_user',  # 덮어쓰기
-    'accounts.social.update_profile_image',  # 추가
-    'social.pipeline.social_auth.associate_user',
-    'social.pipeline.social_auth.load_extra_data',
-    'social.pipeline.user.user_details')
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',  # 이메일 - 소셜 계정 통합
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
