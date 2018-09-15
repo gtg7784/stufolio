299c07b70f34dec0bdc8a97bce6f2230e5101f7d
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-09-15 22:54:58 +0900

    Add: Uplading feature with upload page

---
+++ frontend/src/pages/Upload.js
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+import { PictureThumbnail } from "components";
+import { Icon, Input, Button } from "semantic-ui-react";
+import { URL as API_URL } from "config";
+import { connect } from "react-redux";
+import "./Upload.css";
+
+class Upload extends Component {
+    state = {
+        images: undefined,
+        images_url: [],
+        tags: ""
+    };
+    _renderPictures = () => {
+        var pics = [];
+        Array.from(this.state.images_url).forEach(pic => {
+            pics.push(
+                <PictureThumbnail picture={pic} size="small" alt="미리보기" />
+            );
+        });
+        return pics;
+    };
+    handleChange = event => {
+        let file = event.target.files;
+        var urls = [];
+        Array.from(file).forEach(pic => {
+            urls.push(URL.createObjectURL(pic));
+        });
+        this.setState({ ...this.state, images: file, images_url: urls });
+    };
+    handleTagChange = event => {
+        this.setState({
+            ...this.state,
+            tags: event.target.value
+        });
+    };
+    setImageDone = () => {
+        console.log(this.props);
+        var images_id = [];
+        var tags = [];
+        Array.from(this.state.tags.split(",")).forEach(target => {
+            tags.push(target.replace(" ", ""));
+        });
+        var i = 0;
+        Array.from(this.state.images).forEach(pic => {
+            let formData = new FormData();
+            formData.append("image", pic);
+            console.log(this.props.store);
+            fetch(API_URL + "articles/images/", {
+                method: "POST",
+                headers: {
+                    Authorization: this.props.store.status.auth
+                },
+                body: formData
+            })
+                .then(response => response.json())
+                .then(json => {
+                    images_id.push(json.id);
+                    i++;
+                })
+                .then(() => {
+                    let formData = new FormData();
+                    formData.append("images_id", JSON.stringify(images_id));
+                    formData.append("tags", JSON.stringify(tags));
+                    formData.append("content", "");
+                    if (i === this.state.images.length) {
+                        fetch(API_URL + "articles/", {
+                            method: "POST",
+                            headers: {
+                                Authorization: this.props.store.status.auth
+                            },
+                            body: formData
+                        });
+                    }
+                });
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <Input
+                    icon="hashtag"
+                    iconPosition="left"
+                    placeholder="태그"
+                    onChange={this.handleTagChange}
+                />
+                <label htmlFor="uploader" className="ui huge inverted button">
+                    <Icon name="add" color="black" />
+                </label>
+                <Button inverted onClick={this.setImageDone}>
+                    <Icon name="check" size="big" color="black" />
+                </Button>
+                {this.state.images_url ? this._renderPictures() : null}
+                <input
+                    id="uploader"
+                    className="filebox"
+                    multiple
+                    type="file"
+                    onChange={this.handleChange}
+                />
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        store: state
+    };
+};
+
+export default connect(mapStateToProps)(Upload);
