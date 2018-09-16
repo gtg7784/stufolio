d99f731b1635192bac6d4935cc8615ef7694af5d
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-09-16 19:02:37 +0900

    Fix: wrong function connection
    image를 요청할때 profile 정보가 반환되는 오류 수정

--- backend/django_apps/custom_profile/urls.py
+++ backend/django_apps/custom_profile/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django_apps.custom_profile import views
 
 urlpatterns = [
-    url(r'^image/(?P<string>[\w\-]+)/$', views.ProfileDetail.as_view()),
+    url(r'^image/(?P<string>[\w\-]+)/$', views.image),
     url(r'^signup/$', views.sign_up),
     url(r'^change/username/$', views.change_username),
     url(r'^(?P<string>[\w\-]+)/$', views.ProfileDetail.as_view()),
