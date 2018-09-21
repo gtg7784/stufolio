abd2460de82249190e1f79d02b479aa2e169d662
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-09-21 22:30:02 +0900

    Update: Register.js
    회원가입 성공 유무 팝업 표시 기능 추가

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
+import { TransitionablePortal, Segment } from "semantic-ui-react";
+
 import { Register as RegisterComponent } from "components";
 import { loginSuccess } from "modules/account";
 import { URL } from "config";
+
+import "pages/Template.css";
 
 class Register extends Component {
     state = {
         input_value_username: "",
         input_value_email: "",
         input_value_password1: "",
-        input_value_password2: ""
+        input_value_password2: "",
+        isOpen: false,
+        popupMessage: ""
     };
     handleSubmit = event => {
         event.preventDefault();
@@ -33,8 +39,13 @@ class Register extends Component {
                     "&password2=" +
                     this.state.input_value_password2
             }).then(response => {
-                if (response.status === 200) {
-                    // signup success
+                if (response.status === 201) {
+                    this.showPopup("회원가입 성공");
+                    setTimeout(() => {
+                        this.props.history.push("/login");
+                    }, 1000);
+                } else {
+                    this.showPopup("회원가입 실패");
                 }
             });
         }
@@ -93,18 +104,48 @@ class Register extends Component {
                     });
             });
     };
+
+    showPopup = message => {
+        this.setState({
+            ...this.state,
+            isOpen: true,
+            popupMessage: message
+        });
+        setTimeout(() => {
+            this.setState({
+                ...this.state,
+                isOpen: false
+            });
+        }, 2000);
+    };
+
     render() {
         return (
-            <div>
-                <RegisterComponent
-                    onSubmit={this.handleSubmit}
-                    onUsernameChange={this.handleInputUsername}
-                    onEmailChange={this.handleInputEmail}
-                    onPassword1Change={this.handleInputPassword1}
-                    onPassword2Change={this.handleInputPassword2}
-                    facebookAppId={2155704744643770}
-                    onFacebookResponse={this.handleSocialLogin}
-                />
+            <div className="center">
+                <div>
+                    <RegisterComponent
+                        onSubmit={this.handleSubmit}
+                        onUsernameChange={this.handleInputUsername}
+                        onEmailChange={this.handleInputEmail}
+                        onPassword1Change={this.handleInputPassword1}
+                        onPassword2Change={this.handleInputPassword2}
+                        facebookAppId={2155704744643770}
+                        onFacebookResponse={this.handleSocialLogin}
+                    />
+                    <button onClick={this.showPopup}>test</button>
+                    <TransitionablePortal open={this.state.isOpen}>
+                        <Segment
+                            style={{
+                                left: "40%",
+                                position: "fixed",
+                                top: "50%",
+                                zIndex: 1000
+                            }}
+                        >
+                            {this.state.popupMessage}
+                        </Segment>
+                    </TransitionablePortal>
+                </div>
             </div>
         );
     }
