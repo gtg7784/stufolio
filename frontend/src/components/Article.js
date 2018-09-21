5dbcf848a2b6d97bbf098f703808cc576ee859af
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-09-21 22:23:12 +0900

    Update: Article.js
    하트 개수 표시 기능 추가, 여러번 표시되는 tag의 prop으로 key 추가, prop id를 article_id prop으로 변경

--- frontend/src/components/Article.js
+++ frontend/src/components/Article.js
@@ -1,24 +1,29 @@
 import React, { Component } from "react";
 import { Button, Icon, Image, Card } from "semantic-ui-react";
-import "pages/Template.css";
 
 import { URL } from "config";
 
+import "pages/Template.css";
+import "components/Article.css";
+
 class Article extends Component {
     state = {
-        isHeart: false
+        isHeart: false,
+        numOfHearts: 0
     };
     componentDidMount() {
         this.setState({
             ...this.state,
-            isHeart: this.props.isHeart
+            isHeart: this.props.isHeart,
+            numOfHearts: this.props.numOfHearts
         });
     }
-
     renderTags = tags => {
+        let i = -1;
         const tagsWithIcon = tags.map(tag => {
+            i++;
             return (
-                <span>
+                <span key={i}>
                     <Icon name="hashtag" />
                     {tag}
                 </span>
@@ -34,7 +39,7 @@ class Article extends Component {
     };
 
     onClickHeart = () => {
-        fetch(URL + "articles/" + this.props.id + "/heart/", {
+        fetch(URL + "articles/" + this.props.article_id + "/heart/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded",
@@ -46,7 +51,8 @@ class Article extends Component {
                 const isHeart = json.isCreated;
                 this.setState({
                     ...this.state,
-                    isHeart: isHeart
+                    isHeart: isHeart,
+                    numOfHearts: json.like_count
                 });
             });
     };
@@ -54,41 +60,45 @@ class Article extends Component {
     render() {
         return (
             <div className="center">
-                <Card>
-                    {this.props.images_id
-                        ? this.renderPictures(this.props.images_id)
-                        : null}
-                    <Card.Content>
-                        <Card.Description>
-                            {this.props.tags
-                                ? this.renderTags(this.props.tags)
-                                : null}
-                        </Card.Description>
-                        <Card.Meta>{this.props.writer}</Card.Meta>
-                        <span>
-                            {this.props.date}
-                            {this.state.isHeart ? (
-                                <Button floated="right" inverted icon>
-                                    <Icon
-                                        size="large"
-                                        name="heart"
-                                        color="red"
-                                        onClick={this.onClickHeart}
-                                    />
-                                </Button>
-                            ) : (
-                                <Button floated="right" inverted icon>
-                                    <Icon
-                                        color="black"
-                                        size="large"
-                                        name="heart outline"
-                                        onClick={this.onClickHeart}
-                                    />
-                                </Button>
-                            )}
-                        </span>
-                    </Card.Content>
-                </Card>
+                <div className="article">
+                    <Card>
+                        {this.props.images_id
+                            ? this.renderPictures(this.props.images_id)
+                            : null}
+                        <Card.Content>
+                            <Card.Description>
+                                {this.props.tags
+                                    ? this.renderTags(this.props.tags)
+                                    : null}
+                            </Card.Description>
+                            <Card.Meta>{this.props.writer}</Card.Meta>
+                            <span>
+                                좋아요: {this.state.numOfHearts}
+                                <br />
+                                {this.props.date}
+                                {this.state.isHeart ? (
+                                    <Button floated="right" inverted icon>
+                                        <Icon
+                                            size="large"
+                                            name="heart"
+                                            color="red"
+                                            onClick={this.onClickHeart}
+                                        />
+                                    </Button>
+                                ) : (
+                                    <Button floated="right" inverted icon>
+                                        <Icon
+                                            color="black"
+                                            size="large"
+                                            name="heart outline"
+                                            onClick={this.onClickHeart}
+                                        />
+                                    </Button>
+                                )}
+                            </span>
+                        </Card.Content>
+                    </Card>
+                </div>
             </div>
         );
     }
