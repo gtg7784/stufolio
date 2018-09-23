930f69b4508d8cd092ee8d7e0de502beac5deb56
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-09-23 21:57:29 +0900

    Update: Profile.js
    Profile 컴포넌트를 stateless 에서 state가 있는 컴포넌트로 변경

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,18 +1,37 @@
-import React from "react";
-import { Card, Image } from "semantic-ui-react";
+import React, { Component } from "react";
+import { Card, Image, Icon } from "semantic-ui-react";
 import "components/Profile.css";
+import "pages/Template.css";
 
-const Profile = ({ img_source, username, bio, school }) => (
-    <div>
-        <Card>
-            <Image src={img_source} />
-            <Card.Content>
-                <Card.Header>{username}</Card.Header>
-                <Card.Meta>{bio}</Card.Meta>
-                <Card.Meta>{school}</Card.Meta>
-            </Card.Content>
-        </Card>
-    </div>
-);
+class Profile extends Component {
+    state = {
+        isError: false
+    };
+    render() {
+        return (
+            <div>
+                <Card>
+                    <div className="center profile">
+                        {this.state.isError ? (
+                            <Icon name="user" size="huge" />
+                        ) : (
+                            <Image
+                                src={this.props.img_source}
+                                onError={() => {
+                                    this.setState({ isError: true });
+                                }}
+                            />
+                        )}
+                    </div>
+                    <Card.Content>
+                        <Card.Header>{this.props.username}</Card.Header>
+                        <Card.Meta>{this.props.bio}</Card.Meta>
+                        <Card.Meta>{this.props.school}</Card.Meta>
+                    </Card.Content>
+                </Card>
+            </div>
+        );
+    }
+}
 
 export default Profile;
