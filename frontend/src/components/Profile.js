f7447f7ad079a825cb4be43899e5062982c57d21
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-10-04 01:04:39 +0900

    Update: Profile.js
    - Add day stroke feature
    - Add Calendar Heat Map space
    - chanage the variable name from isImageError from isError

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,36 +1,52 @@
 import React, { Component } from "react";
-import { Card, Image, Icon } from "semantic-ui-react";
+import { Card, Image, Icon, Grid } from "semantic-ui-react";
+
 import "components/Profile.css";
 import "pages/Template.css";
 
 class Profile extends Component {
     state = {
-        isError: false
+        isImageError: false
     };
+
     render() {
         return (
             <div>
-                <Card>
-                    <div>
-                        {this.state.isError ? (
-                            <div className="center profile">
-                                <Icon name="user" size="huge" />
+                <Grid celled>
+                    <Grid.Column width={4}>
+                        <Card>
+                            <div>
+                                {this.state.isImageError ? (
+                                    <div className="profile">
+                                        <Icon name="user" size="huge" />
+                                    </div>
+                                ) : (
+                                    <Image
+                                        src={this.props.imgSource}
+                                        onError={() => {
+                                            this.setState({
+                                                isImageError: true
+                                            });
+                                        }}
+                                    />
+                                )}
                             </div>
-                        ) : (
-                            <Image
-                                src={this.props.img_source}
-                                onError={() => {
-                                    this.setState({ isError: true });
-                                }}
-                            />
-                        )}
-                    </div>
-                    <Card.Content>
-                        <Card.Header>{this.props.username}</Card.Header>
-                        <Card.Meta>{this.props.bio}</Card.Meta>
-                        <Card.Meta>{this.props.school}</Card.Meta>
-                    </Card.Content>
-                </Card>
+                            <Card.Content>
+                                <Card.Header>{this.props.username}</Card.Header>
+                                <Card.Meta>{this.props.bio}</Card.Meta>
+                                <Card.Meta>{this.props.school}</Card.Meta>
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
+                    <Grid.Column width={6}>
+                        <div style={{ width: "600px" }}>
+                            {this.props.calendarHeatMap}
+                        </div>
+                    </Grid.Column>
+                </Grid>
+                <div className="center">
+                    {this.props.dayStroke}일 연속으로 공부중!
+                </div>
             </div>
         );
     }
