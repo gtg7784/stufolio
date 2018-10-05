0325b1cd3f9f4c3cf6f335bf9e55ff9c9ba33d11
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-10-06 00:46:23 +0900

    Update: index.js

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -4,3 +4,4 @@ export { default as Profile } from "./Profile";
 export { default as PictureThumbnail } from "./PictureThumbnail";
 export { default as Article } from "./Article";
 export { default as ProfileManager } from "./ProfileManager";
+export { default as Header } from "./Header";
