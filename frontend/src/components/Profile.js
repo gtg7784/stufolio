2d701e3eddeba8b7ec2e6801e55821940ed11a44
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-10-05 23:00:29 +0900

    Update: Profile.js
    모바일 환경에서는 calendar heatmap이 표시되지 않도록 반응형으로 변경

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card, Image, Icon, Grid } from "semantic-ui-react";
+import { Card, Image, Icon, Grid, Responsive } from "semantic-ui-react";
 
 import "components/Profile.css";
 import "pages/Template.css";
@@ -12,39 +12,71 @@ class Profile extends Component {
     render() {
         return (
             <div>
-                <Grid celled>
-                    <Grid.Column width={4}>
-                        <Card>
-                            <div>
-                                {this.state.isImageError ? (
-                                    <div className="profile">
-                                        <Icon name="user" size="huge" />
-                                    </div>
-                                ) : (
-                                    <Image
-                                        src={this.props.imgSource}
-                                        onError={() => {
-                                            this.setState({
-                                                isImageError: true
-                                            });
-                                        }}
-                                    />
-                                )}
+                <Responsive minWidth={768}>
+                    <Grid celled>
+                        <Grid.Column width={4}>
+                            <Card>
+                                <div>
+                                    {this.state.isImageError ? (
+                                        <div className="profile image">
+                                            <Icon name="user" size="huge" />
+                                        </div>
+                                    ) : (
+                                        <Image
+                                            src={this.props.imgSource}
+                                            onError={() => {
+                                                this.setState({
+                                                    isImageError: true
+                                                });
+                                            }}
+                                        />
+                                    )}
+                                </div>
+                                <Card.Content>
+                                    <Card.Header>
+                                        {this.props.username}
+                                    </Card.Header>
+                                    <Card.Meta>{this.props.bio}</Card.Meta>
+                                    <Card.Meta>{this.props.school}</Card.Meta>
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+                        <Grid.Column width={6}>
+                            <div style={{ width: "600px" }}>
+                                {this.props.calendarHeatMap}
                             </div>
-                            <Card.Content>
-                                <Card.Header>{this.props.username}</Card.Header>
-                                <Card.Meta>{this.props.bio}</Card.Meta>
-                                <Card.Meta>{this.props.school}</Card.Meta>
-                            </Card.Content>
-                        </Card>
-                    </Grid.Column>
-                    <Grid.Column width={6}>
-                        <div style={{ width: "600px" }}>
-                            {this.props.calendarHeatMap}
+                        </Grid.Column>
+                    </Grid>
+                </Responsive>
+                <Responsive maxWidth={768} className="center">
+                    <Card style={{ marginTop: "2rem" }}>
+                        <div>
+                            {this.state.isImageError ? (
+                                <div className="profile image">
+                                    <Icon name="user" size="huge" />
+                                </div>
+                            ) : (
+                                <Image
+                                    src={this.props.imgSource}
+                                    onError={() => {
+                                        this.setState({
+                                            isImageError: true
+                                        });
+                                    }}
+                                />
+                            )}
                         </div>
-                    </Grid.Column>
-                </Grid>
-                <div className="center">
+                        <Card.Content>
+                            <Card.Header>{this.props.username}</Card.Header>
+                            <Card.Meta>{this.props.bio}</Card.Meta>
+                            <Card.Meta>{this.props.school}</Card.Meta>
+                        </Card.Content>
+                    </Card>
+                </Responsive>
+                <div
+                    className="center"
+                    style={{ paddingTop: "2rem", fontSize: "1.2rem" }}
+                >
                     {this.props.dayStroke}일 연속으로 공부중!
                 </div>
             </div>
